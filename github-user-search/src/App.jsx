a056e3a0e0f3c52096bba118a0e03686afe58473
Bugwhisperer004 <njoguvanessa14@gmail.com>
2025-08-01 13:47:18 +0300

    React setup for GitHub user search app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>GitHub User Search</h1>
+      <p>Start typing to search for a GitHub user.</p>
+    </div>
+  );
+};
+
+export default App;
+
