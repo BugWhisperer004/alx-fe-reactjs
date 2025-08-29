a0fa404fb9d1bfd29938b90d1ff3800c9ceaafee
Bugwhisperer004 <njoguvanessa14@gmail.com>
2025-08-29 14:08:21 +0300

    Move todo components into src directory and fix imports

--- react-todo/src/TodoList.test.js
+++ react-todo/src/TodoList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import TodoList from '../components/TodoList';
+import TodoList from './TodoList';
 
 describe('TodoList component', () => {
     test('renders initial todos', () => {
